fc2ed950aa98ff56e49caa6b7a931b8034e19b99
Norman Wijaya <norman.emry@gmail.com>
2015-06-10 15:13:21 +0700

    Chapter 9

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
+  attr_reader :seconds
 
+  def initialize()
+    @seconds = 0
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    "#{"%.2i" %(hour=seconds/3600)}:#{"%.2i" %(minute=(seconds-hour*3600)/60)}:#{"%.2i" %(seconds-hour*3600-minute*60)}"
+  end
 end
